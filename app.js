447b1fa2cd4fce6712c2ba52e9e1d7f8a46b401f
Franek-86 <fraleccese86@gmail.com>
2023-06-21 10:42:09 +0200

    test

--- app.js
+++ app.js
@@ -8,18 +8,20 @@ const connectDB = require("./db/connectDB");
 const authMiddleware = require("./middleware/authMiddleware");
 const errorHandler = require("./middleware/errorHandler");
 const { prototype } = require("./errors/unauthorized");
-const yaml = require("js-yaml");
-const fs = require("fs");
-const swaggerUi = require("swagger-ui-express");
+// const yaml = require("js-yaml");
+// const fs = require("fs");
+// const swaggerUi = require("swagger-ui-express");
 
-const swaggerDocument = yaml.load(fs.readFileSync("./swagger.yaml", "utf8"));
+// const swaggerDocument = yaml.load(fs.readFileSync("./swagger.yaml", "utf8"));
 
-app.use("/", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+// app.use("/", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(express.json());
 app.use("/api/v1/", authRoute);
 app.use("/api/v1/jobs", authMiddleware, jobsRoute);
 app.use(errorHandler);
-
+app.use("/", (req, res) => {
+  res.send("ciao");
+});
 const url = process.env.MONGO_URI;
 let port = process.env.PORT || 3000;
 
